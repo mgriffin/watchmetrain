395b75090d1199b9aafa99fb07b6c8207055f9f4
Mike Griffin <griffinm@gmail.com>
2014-01-03 15:35:21 +0000

    Adds some simple tests

---
+++ tests/mg_test.rb
@@ -0,0 +1,16 @@
+require 'mg.rb'
+require 'test/unit'
+require 'rack/test'
+
+class MGTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_about
+    get '/about'
+    assert_match /Kildare/, last_response.body
+  end
+end
