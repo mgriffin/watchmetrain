85faf77bea8e1d7deac323936008831d74a6c30b
Mike Griffin <griffinm@gmail.com>
2014-01-04 13:13:42 +0000

    Adds some tests for the article model

---
+++ test/article_model_test.rb
@@ -0,0 +1,17 @@
+require 'test/test_helper.rb'
+require 'db.rb'
+require 'data/models.rb'
+
+class ArticleModelTest < Test::Unit::TestCase
+  def setup
+    @article = Article.first
+  end
+
+  def test_date
+    assert_equal 'Monday, October 26, 2009', @article.date
+  end
+
+  def test_summarise
+    assert_match /<p>I started it/, @article.summarise
+  end
+end
