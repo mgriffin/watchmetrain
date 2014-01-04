932a0b97f7c34fe0201aefa560e8a16241245b62
Mike Griffin <griffinm@gmail.com>
2014-01-04 16:15:15 +0000

    Adds a test for tagged articles, with data added in to the import

--- test/article_model_test.rb
+++ test/article_model_test.rb
@@ -18,4 +18,14 @@ class ArticleModelTest < Test::Unit::TestCase
   def test_published
     assert @article.published?
   end
+
+  def test_tagged
+    tagged_article = Article.tagged('run')
+    assert_equal 1, tagged_article[0][:id]
+  end
+
+  def test_tagged_with_an_invalid_tag
+    tagged_article = Article.tagged('narf')
+    assert_equal [], tagged_article
+  end
 end
