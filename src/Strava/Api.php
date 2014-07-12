24b00771a979b0cf57ab4d20c49747ed858a4b62
Mike Griffin <griffinm@gmail.com>
2014-07-12 11:18:23 +0100

    Adds a class to access strava information with guzzle
    This will allow me to download run and ride data from strava through
    cron so I won't have to manually update it any more

---
+++ src/Strava/Api.php
@@ -0,0 +1,42 @@
+<?php
+namespace WMT\Strava;
+
+class Api
+{
+    protected $client;
+    protected $token;
+
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client(['base_url' => 'https://www.strava.com/api/v3/']);
+        $this->token = 'fcd7c9c35565209c3a80337271419385aba0d655';
+    }
+
+    public function getAthlete()
+    {
+        return $this->sendRequest('athlete');
+    }
+
+    public function getActivityById($id)
+    {
+        return $this->sendRequest('activities/' . $id);
+    }
+
+    public function getAllActivities($page = 1)
+    {
+        return $this->sendRequest('athlete/activities?page=' . $page);
+    }
+
+    protected function sendRequest($query)
+    {
+        try {
+            $response = $this->client->get($query, ['query' => ['access_token' => $this->token]]);
+            return $response->json();
+        } catch (RequestException $e) {
+            echo $e->getRequest();
+            if ($e->hasResponse()) {
+                echo $e->getResponse();
+            }
+        }
+    }
+}
